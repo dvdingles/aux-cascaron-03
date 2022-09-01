271c2170138c5b4bb5c7e46ee73e58111e2d6196
dvdapogeo <dvdingles@hotmail.com>
2022-09-01 09:20:28 +0200

    añado nuevo archivo

--- components/Dav.js
+++ components/Dav.js
@@ -6,31 +6,29 @@ import React, { useRef } from "react";
 import { useGLTF } from "@react-three/drei";
 
 export function Dav(props) {
-  const { nodes, materials } = useGLTF("/5D BAURFE---para despliegue.gltf");
+  const { nodes, materials } = useGLTF("/6D BAURFE---para despliegue.gltf");
   return (
     <group {...props} dispose={null}>
-      <group position={[0, 0, -0.01]}>
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom"].geometry}
-          material={materials.TQ_ARD_front}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom_1"].geometry}
-          material={materials.TQ_ARD_back}
-        />
-        <mesh
-          castShadow
-          receiveShadow
-          geometry={nodes["TQ_ARD-geom_2"].geometry}
-          material={materials.TQ_ARD_edge}
-        />
-      </group>
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["TQ_ARD-geom"].geometry}
+        material={materials.TQ_ARD_front}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["TQ_ARD-geom_1"].geometry}
+        material={materials.TQ_ARD_back}
+      />
+      <mesh
+        castShadow
+        receiveShadow
+        geometry={nodes["TQ_ARD-geom_2"].geometry}
+        material={materials.TQ_ARD_edge}
+      />
     </group>
   );
 }
 
-useGLTF.preload("/5D BAURFE---para despliegue.gltf");
+useGLTF.preload("/6D BAURFE---para despliegue.gltf");
